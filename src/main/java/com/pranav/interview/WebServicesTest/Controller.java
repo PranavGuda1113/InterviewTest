10f4b3880df11d2954a6befd1f25192e5b5004ff
PranavGuda <39254936+PranavGuda1113@users.noreply.github.com>
2021-05-20 22:08:38 -0400

    updated changes

--- src/main/java/com/pranav/interview/WebServicesTest/Controller.java
+++ src/main/java/com/pranav/interview/WebServicesTest/Controller.java
@@ -9,10 +9,14 @@ import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
 
 @RestController
 @RequestMapping(value="/api/breeds")
@@ -21,19 +25,31 @@ public class Controller {
 	@GetMapping(value="/list/all", produces="application/json")
 	public JSONObject getAll() {
 		JSONParser parser = new JSONParser();
+		String result = "";
+		Gson resultJSON = new Gson();
 		try {
 			//InputStream is = new ClassPathResource("input.json").getInputStream();
-			File file = new File(getClass().getResource("input.json").getFile());
-			Object obj = parser.parse(new FileReader(file));
-			JSONObject jsonObject = (JSONObject) obj;
-			return Collections.sort(jsonObject);
+			//File file = new File(getClass().getResource("input.json").getFile());
+			//Object obj = parser.parse(new FileReader("input.json"));
+			//JSONObject jsonObject = (JSONObject) obj;
+			RestTemplate rest = new RestTemplate();
+			result = rest.getForObject("https://raw.githubusercontent.com/mlenze/CodingExcercise-Java/main/apidata.json", String.class);
+			//return Collections.sort(jsonObject);
  			//JSONArray companyList = (JSONArray) jsonObject.get("Company List");
-
-
+			
+			return (JSONObject) parser.parse("{\"message\":"+result+",\"status\":\"success\"}");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return null;
+	}
+	
+	//@Override
+	public int compare(JSONObject o1, JSONObject o2) {
+	    String v1 = (String) ((JSONObject) o1.get("attributes")).get("COMMERCIALNAME_E");
+	    String v3 = (String) ((JSONObject) o2.get("attributes")).get("COMMERCIALNAME_E");
+	    return v1.compareTo(v3);
 	}
 	
 	
